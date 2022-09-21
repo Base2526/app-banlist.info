10033f38117277be4bdef27a1f82c4320eb4e3f0
296204 <296204@ho.bigc.co.th>
2022-09-21 14:53:19 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const App= () => {
+  return (
+    <SafeAreaView>
+      <Text>Banlist info</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,2 @@
+Generate
+keytool -genkeypair -v -storetype PKCS12 -keystore banlistinfo.keystore -alias banlistinfo -keyalg RSA -keysize 2048 -validity 10000
